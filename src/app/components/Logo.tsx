9f5162ce3818c431b506d200e95318ae4c7ec8af
Krystian Blaszczyk <krystian.blaszczyk@venturelabs.team>
2026-05-07 12:32:01 +0200

    Logo fix

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
-const logoLight = "src/assets/8a6609da19e33c9cb01930656c8f65f228118571.png";
-const logoDark = "src/assets/204393461263049c2f30de2d3b17be3a2d9cddd4.png";
+const logoLight = "../src/assets/8a6609da19e33c9cb01930656c8f65f228118571.png";
+const logoDark = "../src/assets/204393461263049c2f30de2d3b17be3a2d9cddd4.png";
 
 interface LogoProps {
   className?: string;
